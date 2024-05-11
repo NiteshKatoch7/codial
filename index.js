d45a9e8ed8031eb48d0605f669510cf827fead11
nitesh <nitesh@wurkr.io>
2024-05-11 20:25:10 +0530

    serving the static asset files based on environments

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+
+require('./config/view-helpers')(app);
+
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
 const cookieParser = require('cookie-parser');
