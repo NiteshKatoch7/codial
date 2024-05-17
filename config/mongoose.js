0d19f3606198f896573fccdf666a5cf9e5f28ac7
nitesh <nitesh@wurkr.io>
2024-05-17 23:50:38 +0530

    main

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,12 +1,11 @@
 const mongoose = require('mongoose');
 const env = require('./environment');
 
-mongoose.connect(`${env.db_url}/${env.db}`);
+mongoose.connect(`${env.db_url}/${env.db}`, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
-
 
 db.once('open', function(){
     console.log('Connected to Database :: MongoDB');
